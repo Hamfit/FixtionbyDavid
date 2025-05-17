05e385a472653c4f9058034bfc55f5cdedd03e85
Hamfit <opefawazademolar@gmail.com>
2025-05-17 14:07:12 +0100

    Changed name of organisation

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,10 +19,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src =  "/assets/images/logo/David Logo.jpg";
+      logo.src =  "/assets/images/logo/eemagin 01_092652.svg"; 
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "/assets/images/logo/David Logo.jpg" ;
+      logo.src = "/assets/images/logo/eemagin 01_092652.svg" ;
     }
 
     // show or hide the back-top-top button
